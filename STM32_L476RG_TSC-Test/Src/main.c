bd64e9ab5e203794741484b4a0a1434a310aaca0
michael.machate <michael.machate@LT252114.pns.local>
2018-11-14 14:48:46 +0100

    tests added

--- STM32_L476RG_TSC-Test/Src/main.c
+++ STM32_L476RG_TSC-Test/Src/main.c
@@ -258,12 +258,12 @@ static void MX_TSC_Init(void) {
 	/**Configure the TSC peripheral
 	 */
 	htsc.Instance = TSC;
-	htsc.Init.CTPulseHighLength = TSC_CTPH_2CYCLES;
-	htsc.Init.CTPulseLowLength = TSC_CTPL_2CYCLES;
+	htsc.Init.CTPulseHighLength = TSC_CTPH_16CYCLES;
+	htsc.Init.CTPulseLowLength = TSC_CTPL_16CYCLES;
 	htsc.Init.SpreadSpectrum = ENABLE;
-	htsc.Init.SpreadSpectrumDeviation = 1;
+	htsc.Init.SpreadSpectrumDeviation = 127;
 	htsc.Init.SpreadSpectrumPrescaler = TSC_SS_PRESC_DIV1;
-	htsc.Init.PulseGeneratorPrescaler = TSC_PG_PRESC_DIV4;
+	htsc.Init.PulseGeneratorPrescaler = TSC_PG_PRESC_DIV128;
 	htsc.Init.MaxCountValue = TSC_MCV_8191;
 	htsc.Init.IODefaultMode = TSC_IODEF_OUT_PP_LOW;
 	htsc.Init.SynchroPinPolarity = TSC_SYNC_POLARITY_FALLING;
